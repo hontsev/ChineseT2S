8f222af56fa7d31a9ab5b8a32b2f8c31cbb1ceb2
hontsev <hontsev@gmail.com>
2017-10-11 14:02:16 +0800

    all files

---
+++ ChineseT2S/T2SUtility.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ChineseT2S
+{
+    public static class T2SUtility
+    {
+
+        /// <summary>
+        /// 查表去掉异体字
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static string RemoveVariant(string source)
+        {
+            string res = source;
+
+            try
+            {
+                string[] pairs = Properties.Resources.v2t.Split('|');
+                Dictionary<string, string> changepairs = new Dictionary<string, string>();
+                foreach (var p in pairs)
+                {
+                    string[] pair = p.Split(',');
+                    if (!changepairs.ContainsKey(pair[1]))
+                    {
+                        changepairs[pair[1]] = pair[0];
+                    }
+                }
+                string tmp = "";
+                foreach (var c in res)
+                {
+                    if (changepairs.ContainsKey(c.ToString())) tmp += changepairs[c.ToString()];
+                    else tmp += c;
+                }
+                res = tmp;
+            }
+            catch(Exception e)
+            {
+                throw new Exception(string.Format("异体字转换繁体字时失败:{0}",e.Message));
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// 查表去掉繁体字
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static string RemoveTraditional(string source)
+        {
+            string res = source;
+
+            try
+            {
+                string[] pairs = Properties.Resources.s2t.Split('|');
+                Dictionary<string, string> changepairs = new Dictionary<string, string>();
+                foreach (var p in pairs)
+                {
+                    string[] pair = p.Split(',');
+                    if (!changepairs.ContainsKey(pair[1]))
+                    {
+                        changepairs[pair[1]] = pair[0];
+                    }
+                }
+                string tmp = "";
+                foreach (var c in res)
+                {
+                    if (changepairs.ContainsKey(c.ToString())) tmp += changepairs[c.ToString()];
+                    else tmp += c;
+                }
+                res = tmp;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Format("查表转换繁体字时失败:{0}", e.Message));
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// 转为简体
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string ToSimplified(string source)
+        {
+            try
+            {
+                // 先用VB方法转成简体
+                string res = Microsoft.VisualBasic.Strings.StrConv(source, VbStrConv.SimplifiedChinese, 0);
+
+                // 将异体字转化为简体字
+                res = RemoveVariant(res);
+
+                // 再用查表的方法去除未被转化的繁体字
+                res = RemoveTraditional(res);
+
+                return res;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Format("转换失败:{0}", e.Message));
+            }       
+        }
+
+        /// <summary>
+        /// 转为繁体
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string ToTraditional(string source)
+        {
+            string target = Microsoft.VisualBasic.Strings.StrConv(source, VbStrConv.TraditionalChinese, 0);
+            return target;
+        }
+    }
+}
---
+++ TestForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ChineseT2S;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ori = textBox1.Text;
+            try
+            {
+                textBox2.Text = T2SUtility.ToSimplified(ori);
+            }
+            catch(Exception ex)
+            {
+                textBox2.Text = ex.Message;
+            }
+        }
+    }
+}
